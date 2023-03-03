a1730403e9c4378b24bf50ac8776148e7ef0df86
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-03-03 14:23:31 +0530

    OOP Des - tut finished
    Finished the second scene of the program from the tutorial.

--- OOP-des/Day04/radio-buttons/src/main/java/com/example/multipleviews/HelloController.java
+++ OOP-des/Day04/radio-buttons/src/main/java/com/example/multipleviews/HelloController.java
@@ -5,10 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloController {
 
@@ -20,6 +23,12 @@ public class HelloController {
 
     @FXML
     private Button viewStudents;
+
+    @FXML
+    private Button addStudentAdd;
+
+    @FXML
+    private Button addStudentBack;
 
     @FXML
     void addStudentClick(ActionEvent event) throws IOException {
@@ -54,5 +63,44 @@ public class HelloController {
         primaryStage.show();
 
     }
+
+    @FXML
+    void addStudentAddClick(ActionEvent event) throws IOException {
+        Alert newalert = new Alert(Alert.AlertType.CONFIRMATION);
+        newalert.setContentText("Details were added successfully!");
+        //Optional type variable will hold any type of data type. (Because we are using button type)
+        Optional<ButtonType> result = newalert.showAndWait();
+        ButtonType button = result.orElse(ButtonType.CANCEL);
+        if ( button == ButtonType.OK){
+
+            Stage stage = (Stage) addStudentAdd.getScene().getWindow();
+            stage.close();
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            primaryStage.setTitle("SIMS");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        }
+        else if (button == ButtonType.CANCEL) {
+            System.out.println("It will be cancelled.");
+        }
+    }
+
+    @FXML
+    void addStudentBackClick(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) addStudentBack.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("SIMS");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+
+
 
 }
