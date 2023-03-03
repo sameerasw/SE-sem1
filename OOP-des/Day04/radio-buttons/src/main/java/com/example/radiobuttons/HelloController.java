01ad0189459716920fd75f86bbdd63221a8568b8
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-03-03 12:56:22 +0530

    Finished OOP Des tut
    Finished the multiple scenes tutorial

--- OOP-des/Day04/radio-buttons/src/main/java/com/example/radiobuttons/HelloController.java
+++ OOP-des/Day04/radio-buttons/src/main/java/com/example/radiobuttons/HelloController.java
@@ -1,14 +1,58 @@
 package com.example.radiobuttons;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button addStudent;
+
+    @FXML
+    private Button quit;
+
+    @FXML
+    private Button viewStudents;
+
+    @FXML
+    void addStudentClick(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) addStudent.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("addStudent.fxml"));
+        primaryStage.setTitle("Add Student Details");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
     }
+
+    @FXML
+    void quitClick(ActionEvent event) {
+
+        Stage stage = (Stage) quit.getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    void viewStudentsClick(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) viewStudents.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("viewStudents.fxml"));
+        primaryStage.setTitle("View Student Details");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
 }
